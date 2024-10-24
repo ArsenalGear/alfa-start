b09e6e443dc66a1c70ffcc1c15e0899744f53a72
aboldyrev <aboldyrev@colvir.com>
2024-10-24 16:45:36 +0300

    feat(add route): add route in project 102

--- src/desktop/components/page-header/page-header.tsx
+++ src/desktop/components/page-header/page-header.tsx
@@ -1,21 +1,21 @@
 import React from 'react';
-import {useDispatch, useSelector} from 'react-redux';
-import {PeriodSelection} from '@alfabank/alfapeople-components/period-selection';
+import { useDispatch, useSelector } from 'react-redux';
+import { PeriodSelection } from '@alfabank/alfapeople-components/period-selection';
 
-import {Skeleton} from '@alfalab/core-components/skeleton/cssm';
-import {Space} from '@alfalab/core-components/space/cssm';
-import {Typography} from '@alfalab/core-components/typography/cssm';
+import { Skeleton } from '@alfalab/core-components/skeleton/cssm';
+import { Space } from '@alfalab/core-components/space/cssm';
+import { Typography } from '@alfalab/core-components/typography/cssm';
 
-import {useAmountDetailsStatus} from '../../../shared/hooks/use-amount-details-status';
-import {useGetPaymentPeriodsQueryState} from '../../../shared/slices/api-slice';
-import {selectCurrentPeriodId, setCurrentPeriodId} from '../../../shared/slices/app-slice';
+import { useAmountDetailsStatus } from '../../../shared/hooks/use-amount-details-status';
+import { useGetPaymentPeriodsQueryState } from '../../../shared/slices/api-slice';
+import { selectCurrentPeriodId, setCurrentPeriodId } from '../../../shared/slices/app-slice';
 
 import styles from './page-header.module.css';
 
 export const PageHeader: React.FC = () => {
     const dispatch = useDispatch();
-    const {data: paymentPeriods = [], isFetching} = useGetPaymentPeriodsQueryState();
-    const {isFetching: isAmountsDetailsFetching, isSuccess: isAmountsDetailsSuccess} =
+    const { data: paymentPeriods = [], isFetching } = useGetPaymentPeriodsQueryState();
+    const { isFetching: isAmountsDetailsFetching, isSuccess: isAmountsDetailsSuccess } =
         useAmountDetailsStatus();
     const currentPeriodId = useSelector(selectCurrentPeriodId);
     const currentPeriodIndex = paymentPeriods.findIndex((it) => it.periodId === currentPeriodId);
@@ -38,12 +38,12 @@ export const PageHeader: React.FC = () => {
         <div className={styles.header}>
             <Space direction='horizontal' size={8} align='center'>
                 <Typography.Title tag='h1' view='medium'>
-                    Пример1 Stab Remote page-header.tsx
+                    Пример1 Stab Remote page-header.tsx1
                 </Typography.Title>
             </Space>
             {isFetching ? (
                 <Skeleton visible={true}>
-                    <div style={{width: '200px', height: '24px'}}/>
+                    <div style={{ width: '200px', height: '24px' }} />
                 </Skeleton>
             ) : (
                 <React.Fragment>
